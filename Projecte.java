60c54200efdb8937d14e69ea63595f1e6b47fe8e
JoanJuan10 <37046841+JoanJuan10@users.noreply.github.com>
2019-01-14 20:26:28 +0100

    Crear Esquema de Joc

--- Projecte.java
+++ Projecte.java
@@ -5,13 +5,34 @@ public class Projecte {
 	public static void main(String[] args) {
 
 		Scanner teclado = new Scanner(System.in);
-		
+
 		// Menu
 		System.out.println("Selecciona:");
 		System.out.println("1 - Per a jugar");
 		System.out.println("2 - Per a veure els records");
 		System.out.println("3 - Per a sortir");
 		int seleccio = teclado.nextInt();
+
+		boolean ejecucion = true;
+		while (ejecucion) {
+			if (seleccio == 1) {
+				joc();
+			}
+			else if (seleccio == 2) {
+				veureRecords();
+			}
+			else {
+				ejecucion = false;
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		char[][] matrizTablero =     {{'-','○','-','○','-','○','-','○'},
                 					{'○','-','○','-','○','-','○','-'},
@@ -41,6 +62,105 @@ public class Projecte {
 			System.out.println();
 		}
 	}
-
+	public static void joc() {
+		// Variables Inicials
+		
+		
+		// Comença Torn Negres
+		
+		
+				// Imprimim Taula
+			
+			
+				// Demanem Posició que es vol moure (Fila + Columna)
+			
+			
+				// Comprobem que es del seu color
+				
+			
+				// Demanem Posició on es vol moure (Fila + Columna)
+			
+			
+				// Comprobem que el moviment es legal
+			
+			
+				// Comprobem que no esta ocupat
+				
+				
+				// Comprobem si hi ha una altre peça que mata
+				
+				
+				// Movem la fitxa
+				
+				
+				// Comprobem si mata
+				// Si mata, matar
+				
+				// Un cop moguda, comprobem si torna a matar
+				
+				
+				// Si pot matar, matar un altre cop
+		
+		
+		
+		// Comença Torn Blanques
+		
+		
+				// Imprimim Taula
+		
+		
+				// Demanem Posició que es vol moure (Fila + Columna)
+				
+				
+				// Comprobem que es del seu color
+				
+				
+				// Demanem Posició on es vol moure (Fila + Columna)
+				
+				
+				// Comprobem que el moviment es legal
+				
+				
+				// Comprobem que no esta ocupat
+				
+				
+				// Comprobem si hi ha una altre peça que mata
+				
+				
+				// Movem la fitxa
+				
+				
+				// Comprobem si mata
+				// Si mata, matar
+				
+				// Un cop moguda, comprobem si torna a matar
+				
+				
+				// Si pot matar, matar un altre cop
+				
+	}
+	
+	
+	public static void veureRecords() {
+		
+	}
+	public static void movimentBlanc(int filaPrincipi, int columnaPrincipi, int filaFinal, int ColumnaFinal, char[][] matrizTablero) {
+		
+	}
+	public static void movimentNegre(int filaPrincipi, int columnaPrincipi, int filaFinal, int ColumnaFinal, char[][] matrizTablero) {
+		
+	}
+	public static void movimentCoronat(int color,int filaPrincipi, int columnaPrincipi, int filaFinal, int ColumnaFinal, char[][] matrizTablero) {
+		
+	}
+	public static void coronacio(int color, int fila, int columna, char[][] matrizTablero) {
+		
+	}
+	public static boolean colocacioCorrecta(int fila, int columna, char[][] matrizTablero) {
+		
+		return true;
+	}
+	public static void matar(int color, int filaMatada, int columnaMatada, char[][] matrizTablero) {
+		
+	}
 }
-
