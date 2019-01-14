92878beebfcf2d078ddbf0b1781de4d6f05e19db
kitajima20 <46686458+kitajima20@users.noreply.github.com>
2019-01-14 19:33:49 +0100

    Update Projecte.java

--- Projecte.java
+++ Projecte.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class Projecte {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Selecciona:");
+		System.out.println("1 - Per a jugar");
+		System.out.println("2 - Per a veure els records");
+		System.out.println("3 - Per a sortir");
+		int seleccio = teclado.nextInt();
+		
 	}
 
 }
+
